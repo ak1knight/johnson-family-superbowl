f37960a41e6c23eb4d57463099b56edf6f3e5cee
Alex Knight <alex.knight@leavittpartners.com>
2020-01-10 15:08:57 -0700

    Updating config trying to get it to work on the server

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ module.exports = {
         config.node = {
             fs: 'empty'
         }
+
+        if (!process.env.BUNDLE_AWS_SDK) {
+            config.externals = config.externals || [];
+            config.externals.push({ "aws-sdk": "aws-sdk" });
+        } else {
+            console.warn("Bundling aws-sdk. Only doing this in development mode");
+        }
+        
         return config
     }
 }
