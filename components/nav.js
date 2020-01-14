3b62102b480a1823bcdfff24dd5f0d36a4e9868d
Alex Knight <alex.knight@leavittpartners.com>
2020-01-14 15:58:38 -0700

    Updated Nav Colors

--- components/nav.js
+++ components/nav.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
+import Router from "next/router";
 
 const Nav = () => (
-    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
             <a className="navbar-brand mb-0 h1" href="#">Johnson Family Super Bowl Pool</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,15 +12,21 @@ const Nav = () => (
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <Link href="/"><li className="nav-item">
-                        <span className="nav-link">Entry Form <span className="sr-only">(current)</span></span>
-                    </li></Link>
-                    <Link href="/big_board"><li className="nav-item">
-                        <span className="nav-link">The Big Board</span>
-                    </li></Link>
-                    <Link href="/propbetboard"><li className="nav-item">
-                        <span className="nav-link">Prop Bet Board</span>
-                    </li></Link>
+                    <li className={`nav-item ${Router.route == '/' && 'active'}`}>
+                        <Link href="/">
+                            <a className="nav-link">Entry Form <span className="sr-only">(current)</span></a>
+                        </Link>
+                    </li>
+                    <li className={`nav-item ${Router.route == '/big_board' && 'active'}`}>
+                        <Link href="/big_board">
+                            <a className="nav-link">The Big Board</a>
+                        </Link>
+                    </li>
+                    <li className={`nav-item ${Router.route == '/propbetboard' && 'active'}`}>
+                        <Link href="/propbetboard">
+                            <a className="nav-link">Prop Bet Board</a>
+                        </Link>
+                    </li>
                 </ul>
             </div>
         </div>
