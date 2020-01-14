ad38adc2b9842f6998b580013c1017f259c6e485
Alex Knight <alex.knight@leavittpartners.com>
2020-01-14 15:58:24 -0700

    Changed big board to load asynchronously

---
+++ components/bigboardtable.js
@@ -0,0 +1,61 @@
+import data from '../data'
+import { teams, periodNames } from './entryform'
+import { useState, useEffect } from 'react';
+
+function formatScore(entry, quarter) {
+    const firstTeamScore = entry[teams[0]][quarter].score;
+    const secondTeamScore = entry[teams[1]][quarter].score
+    return firstTeamScore == secondTeamScore ? `${firstTeamScore} - ${secondTeamScore} Tie` 
+                : firstTeamScore > secondTeamScore ? `${firstTeamScore} - ${secondTeamScore} ${teams[0]}` 
+                : `${secondTeamScore} - ${firstTeamScore} ${teams[1]}`
+}
+
+const BigBoardTable = () => {
+    const [entries, setEntries] = useState();
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('/api/entry');
+            setEntries(await response.json());
+        }
+
+        fetchData();
+    }, []);
+
+    return <table className="table border-top-0">
+        <thead>
+            <tr>
+                <th className="border-top-0" scope="col">Name</th>
+                <th className="border-top-0" scope="col">Anthem Time</th>
+                {!!periodNames && periodNames.map(q => <th className="border-top-0" scope="col">{`${q} Score`}</th>)}
+                {!!teams && teams.map(t => <th className="border-top-0" scope="col">{`${t} Yards`}</th>)}
+            </tr>
+        </thead>
+        <tbody>
+            {!!entries && entries.map(e => (
+                <tr>
+                    <th scope="row">{e.entry.name}</th>
+                    <td>{e.entry[0].response}</td>
+                    {periodNames.map(q => <td>{formatScore(e.entry, q)}</td>)}
+                    {teams.map(t => <td>{e.entry[t].yards}</td>)}
+                </tr>
+            ))}
+        </tbody>
+    </table>
+}
+
+BigBoardTable.getInitialProps = async ({ req }) => {
+    if (req) {
+        // this is server side
+        // is fine to use aws-sdk here
+        return {
+            entries: await data.readEntries()
+        };
+    } else {
+        // we are client side
+        // fetch via api
+        const response = await fetch('/api/entry')
+        return { entries: await response.json() }
+    }
+}
+
+export default BigBoardTable
--- pages/big_board.js
+++ pages/big_board.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import { teams, periodNames } from "../components/entryform";
+import dynamic from 'next/dynamic'
 import Layout from '../components/layout';
 import Card from '../components/card';
-import data from '../data';
+import BigBoardTable from "../components/bigboardtable";
 
-function formatScore(entry, quarter) {
-    const firstTeamScore = entry[teams[0]][quarter].score;
-    const secondTeamScore = entry[teams[1]][quarter].score
-    return firstTeamScore == secondTeamScore ? `${firstTeamScore} - ${secondTeamScore} Tie` 
-                : firstTeamScore > secondTeamScore ? `${firstTeamScore} - ${secondTeamScore} ${teams[0]}` 
-                : `${secondTeamScore} - ${firstTeamScore} ${teams[1]}`
-}
 
-const BigBoard = ({ entries }) => {
+const BigBoard = () => {
     return <Layout>
         <div className="jumbotron jumbotron-fluid bg-primary text-white">
             <div className="container">
@@ -21,44 +14,11 @@ const BigBoard = ({ entries }) => {
         </div>
         <div className="container mt-3">
             <Card>
-                <table className="table border-top-0">
-                    <thead>
-                        <tr>
-                            <th className="border-top-0" scope="col">Name</th>
-                            <th className="border-top-0" scope="col">Anthem Time</th>
-                            {periodNames.map(q => <th className="border-top-0" scope="col">{`${q} Score`}</th>)}
-                            {teams.map(t => <th className="border-top-0" scope="col">{`${t} Yards`}</th>)}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {entries.map(e => (
-                            <tr>
-                                <th scope="row">{e.entry.name}</th>
-                                <td>{e.entry[0].response}</td>
-                                {periodNames.map(q => <td>{formatScore(e.entry, q)}</td>)}
-                                {teams.map(t => <td>{e.entry[t].yards}</td>)}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                <BigBoardTable />
             </Card>
         </div>
     </Layout>
 }
 
-BigBoard.getInitialProps = async ({ req }) => {
-    if (req) {
-        // this is server side
-        // is fine to use aws-sdk here
-        return {
-            entries: await data.readEntries()
-        };
-    } else {
-        // we are client side
-        // fetch via api
-        const response = await fetch('/api/entry')
-        return { entries: await response.json() }
-    }
-}
 
 export default BigBoard
