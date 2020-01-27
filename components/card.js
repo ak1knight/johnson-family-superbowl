80600ecd8fe068314aa0132b557030d41c7ef31a
Alex Knight <alex.knight@leavittpartners.com>
2020-01-27 16:03:58 -0700

    Made scrollspy link jump to a nicer spot

--- components/card.js
+++ components/card.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const Card = (props) => (
-    <div id={props.id} className="card mb-3">
-        {!!props.title && <h4 className="card-header">{props.title}</h4>}
+    <div className="card mb-3">
+        {!!props.title && <h4 className="card-header">
+            {!!props.id && <span id={props.id} style={{display: 'block', position: 'absoute', visibility: 'hidden', marginTop: '-23px', paddingTop: '23px'}}></span>}
+            {props.title}
+        </h4>}
         <div className="card-body">
             {props.children}
         </div>
