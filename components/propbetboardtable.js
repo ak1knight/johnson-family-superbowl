6b7da55331a9df6d5cca0c0950c8bf7a6c2252b3
Alex Knight <alex.knight@milliman.com>
2021-02-02 12:42:51 -0700

    Fixing Prop Bet Board for 2021

--- components/propbetboardtable.js
+++ components/propbetboardtable.js
@@ -2,19 +2,22 @@ import data from '../data'
 import { useState, useEffect } from 'react';
 import { questions } from "../data/formdata";
 
-const PropBetBoardTable = () => {
+const PropBetBoardTable = ({year}) => {
     const [entries, setEntries] = useState();
     const [winningEntry, setWinningEntry] = useState();
     
     useEffect(() => {
+        setEntries(null)
+        setWinningEntry(null)
+
         async function fetchData() {
-            const [entryResponse, winningEntryResponse] = await Promise.all([fetch('/api/entry'), fetch('/api/winningentry')]);
+            const [entryResponse, winningEntryResponse] = await Promise.all([fetch(`/api/entry/${year}`), fetch(`/api/winningentry/${year}`)]);
             setEntries(await entryResponse.json());
             setWinningEntry(await winningEntryResponse.json());
         }
 
         fetchData();
-    }, []);
+    }, [year]);
 
     console.log(winningEntry);
 
@@ -24,8 +27,8 @@ const PropBetBoardTable = () => {
 
     if (!!entries) {
         entries.sort((e1, e2) => 
-            questions.slice(1).map((q, i) => !!winningEntry && !!winningEntry.entry[i + 1] && e2.entry[i + 1].response == winningEntry.entry[i + 1].response ? q.options.find(o => o.name == winningEntry.entry[i + 1].response).score : 0).reduce((a,b) => a + b, 0) -
-            questions.slice(1).map((q, i) => !!winningEntry && !!winningEntry.entry[i + 1] && e1.entry[i + 1].response == winningEntry.entry[i + 1].response ? q.options.find(o => o.name == winningEntry.entry[i + 1].response).score : 0).reduce((a,b) => a + b, 0)
+            questions[year].slice(1).map((q, i) => !!winningEntry && winningEntry.yearKey === year && !!winningEntry.entry[i + 1] && e2.entry[i + 1].response == winningEntry.entry[i + 1].response ? q.options.find(o => o.name == winningEntry.entry[i + 1].response).score : 0).reduce((a,b) => a + b, 0) -
+            questions[year].slice(1).map((q, i) => !!winningEntry && winningEntry.yearKey === year && !!winningEntry.entry[i + 1] && e1.entry[i + 1].response == winningEntry.entry[i + 1].response ? q.options.find(o => o.name == winningEntry.entry[i + 1].response).score : 0).reduce((a,b) => a + b, 0)
         )
     }
 
@@ -34,19 +37,22 @@ const PropBetBoardTable = () => {
             <tr>
                 <th className="border-top-0" scope="col"></th>
                 <th className="border-top-0" scope="col">Name</th>
-                {!!questions && questions.slice(1).map((q, i) => <th key={i} className="border-top-0" scope="col">{q.short}</th>)}
+                {!!questions[year] && questions[year].slice(1).map((q, i) => <th key={i} className="border-top-0" scope="col">{q.short}</th>)}
                 <th className="border-top-0" scope="col">Total</th>
             </tr>
         </thead>
         <tbody>
-            {!!entries ? entries.map((e, rank) => (
-                <tr key={e.id} style={rank < 5 ? {backgroundColor: `rgb(${193 + (rank * (62 / 5))},${226 + (rank * (29 / 5))},${255 + (rank * (0 / 5))})`} : {}}>
+            {!!entries ? entries.map((e, i, arr) => {
+                const entryScore = questions[year].slice(1).map((q, i) => !!winningEntry && winningEntry.yearKey === year && !!winningEntry.entry[i + 1] && e.entry[i + 1].response == winningEntry.entry[i + 1].response ? q.options.find(o => o.name == winningEntry.entry[i + 1].response).score : 0).reduce((a,b) => a + b, 0);
+                const rank = arr.slice().map(a => questions[year].slice(1).map((q, i) => !!winningEntry && winningEntry.yearKey === year && !!winningEntry.entry[i + 1] && a.entry[i + 1].response == winningEntry.entry[i + 1].response ? q.options.find(o => o.name == winningEntry.entry[i + 1].response).score : 0).reduce((a,b) => a + b, 0)).indexOf(entryScore);
+
+                return <tr key={e.id} style={rank < 5 ? {backgroundColor: `rgb(${193 + (rank * (62 / 5))},${226 + (rank * (29 / 5))},${255 + (rank * (0 / 5))})`} : {}}>
                     <th scope="row">{rank + 1}</th>
                     <th scope="row">{e.entry.name}</th>
-                    {questions.slice(1).map((q, i) => <td className={!!winningEntry && !!winningEntry.entry[i + 1] && e.entry[i + 1].response == winningEntry.entry[i + 1].response ? 'text-success font-weight-bold' : ''} key={i}>{e.entry[i + 1].response}</td>)}
-                    <td className="font-weight-bold">{questions.slice(1).map((q, i) => !!winningEntry && !!winningEntry.entry[i + 1] && e.entry[i + 1].response == winningEntry.entry[i + 1].response ? q.options.find(o => o.name == winningEntry.entry[i + 1].response).score : 0).reduce((a,b) => a + b, 0)}</td>
+                    {questions[year].slice(1).map((q, i) => <td className={!!winningEntry && winningEntry.yearKey === year && !!winningEntry.entry[i + 1] && e.entry[i + 1].response == winningEntry.entry[i + 1].response ? 'text-success font-weight-bold' : ''} key={i}>{e.entry[i + 1].response}</td>)}
+                    <td className="font-weight-bold">{questions[year].slice(1).map((q, i) => !!winningEntry && winningEntry.yearKey === year && !!winningEntry.entry[i + 1] && e.entry[i + 1].response == winningEntry.entry[i + 1].response ? q.options.find(o => o.name == winningEntry.entry[i + 1].response).score : 0).reduce((a,b) => a + b, 0)}</td>
                 </tr>
-            )) :
+            }) :
             <tr>
                 <td className="d-flex justify-content-center">
                     <div className="spinner-border" role="status">
@@ -57,20 +63,5 @@ const PropBetBoardTable = () => {
         </tbody>
     </table>
 }
-
-// BigBoardTable.getInitialProps = async ({ req }) => {
-//     if (req) {
-//         // this is server side
-//         // is fine to use aws-sdk here
-//         return {
-//             entries: await data.readEntries()
-//         };
-//     } else {
-//         // we are client side
-//         // fetch via api
-//         const response = await fetch('/api/entry')
-//         return { entries: await response.json() }
-//     }
-// }
 
 export default PropBetBoardTable
--- pages/propbetboard.js
+++ pages/propbetboard.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/layout';
 import Card from '../components/card';
 import PropBetBoardTable from '../components/propbetboardtable';
 
 
 const PropBetBoard = () => {
+    const [year, setYear] = useState(2021);
+
     return <Layout>
         <div className="jumbotron jumbotron-fluid bg-primary text-white">
             <div className="container">
@@ -12,9 +14,14 @@ const PropBetBoard = () => {
             </div>
         </div>
         <div className="container mt-3">
+            <div className="btn-toolbar mb-3" role="toolbar">
+                <div className="btn-group mr-2" role="group">
+                    {[2020, 2021].map(y => <button type="button" className={`btn btn-primary ${y === year ? 'active' : ''}`} onClick={() => setYear(y)}>{y}</button>)}
+                </div>
+            </div>
             <Card>
                 <div className="table-responsive">
-                    <PropBetBoardTable />
+                    <PropBetBoardTable year={year} />
                 </div>
             </Card>
         </div>
